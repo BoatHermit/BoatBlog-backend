83d0fa6f44b651ed1776e03f7b434cdc5a7226f1
Yin Zihang <yin.zihang@hotmail.com>
2022-07-26 20:00:02 +0800

    refactor： 将返回热门标签数量提取为常量，将TagsController重命名为TagController

--- src/main/java/com/boathermit/boatblog/controller/TagController.java
+++ src/main/java/com/boathermit/boatblog/controller/TagController.java
@@ -1,6 +1,6 @@
 package com.boathermit.boatblog.controller;
 
-import com.boathermit.boatblog.model.vo.TagVo;
+import com.boathermit.boatblog.enums.Limits;
 import com.boathermit.boatblog.service.TagService;
 import com.boathermit.boatblog.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,27 +8,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 /**
  * @author Yin Zihang
  * @since 2022/7/26 12:16
+ * @see com.boathermit.boatblog.service.impl.TagServiceImpl
  */
 @RestController
 @RequestMapping("/tags")
-public class TagsController {
+public class TagController {
 
     private final TagService tagService;
 
     @Autowired
-    TagsController(TagService tagService) {
+    TagController(TagService tagService) {
         this.tagService = tagService;
     }
 
+    /**
+     * 首页 最热标签列表
+     *
+     * @return 最热标签列表
+     */
     @GetMapping("/hot")
     public Result listHotTags() {
-        int limit = 6;
-        List<TagVo> tagVoList = tagService.hot(limit);
-        return Result.success(tagVoList);
+        return Result.success(tagService.hot(Limits.HOT_TAG_LIMIT.getLimit()));
     }
 }
---
+++ src/main/java/com/boathermit/boatblog/enums/Limits.java
@@ -0,0 +1,20 @@
+package com.boathermit.boatblog.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author Yin Zihang
+ * @since 2022/7/26 19:44
+ */
+@AllArgsConstructor
+@Getter
+public enum Limits {
+
+    //返回的最热标签数量
+    HOT_TAG_LIMIT(6),
+    //返回的最热文章数量
+    HOT_ARTICLE_LIMIT(5);
+
+    private final int limit;
+}
